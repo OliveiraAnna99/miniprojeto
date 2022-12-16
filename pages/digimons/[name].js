969282d82387aa540625da5aa78fef2f2925843c
OliveiraAnna99 <anna.oliveira.700@ufrn.edu.br>
2022-12-16 20:17:58 -0300

    Pequenas Melhorias

--- pages/digimons/[name].js
+++ pages/digimons/[name].js
@@ -4,7 +4,7 @@ import {
     Navbar, NavItem, NavbarToggler, Collapse, NavLink, Nav, NavbarBrand, 
     Form, Table, Button, FormGroup,InputGroup, InputGroupText, InputGroupAddon, Input, Card, CardBody, CardText
 } from 'reactstrap';
-export default function Digimons({data, data2}){
+export default function Digimons({data, data2,pesquisar}){
     var id = 1;
     let contador =0;
     return (
@@ -12,12 +12,21 @@ export default function Digimons({data, data2}){
             <>
             <Navbar className="my-2" color="secondary" dark>
                 <NavbarBrand href='/'>
-                    Reactstrap
+                            DigiNext
                 </NavbarBrand>
+                <Nav>
+                    <NavLink style ={{color: 'white'}} href='/'>
+                        Digimons
+                    </NavLink>
+                    <NavLink style ={{color: 'white'}} href='/contact'>
+                        Contatos
+                    </NavLink>
+                </Nav>
+               
                     <Form method='GET'>
                         <FormGroup>
                           <div class="input-group mb-3">
-                            <Input  placeholder =''/>
+                            <Input  placeholder ='' value={pesquisar}/>
                             <div class="input-group-append">
                                 <Input type='submit' color='primary' value ='Buscar'></Input>
                             </div>
@@ -44,14 +53,14 @@ export default function Digimons({data, data2}){
                     
                       <div>
                         <CardBody>
-                          
+                          <div style={{height: '10rem'}}></div>
                             
                             <div className='container'>
                                 <div className='row'>
                                     <div className='col'>
-                                        <img src={data2[contador].image_url} style={{width: '30%'}}></img>
+                                        <img src={data2[contador].image_url} style={{width: '40%'}}></img>
                                     </div>
-                                    <div className='col'>
+                                    <div className='col' style={{textAlign: 'justify'}}>
                                         Esse é o {data2[contador].name} o digimon do tipo {data2[contador].digi_type}, devido a sua coloração
                                         {data2[contador].color} esse digimon chama bastante atenção entre os digiescolhidos, 
                                         esse digimon possuí como principal atributo, o atributo : {data2[contador].attribute}. Além de estar no level {data2[contador].level} esse
